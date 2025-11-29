27b02b28534f35c133292a17ff8835fcb9fe84e9
jeongwoo78 <poly0533@gmail.com>
2025-11-30 06:45:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/App.css'
-import eruda from 'eruda'
 
-// 모바일 디버깅용 콘솔
-eruda.init()
+// 모바일 디버깅용 콘솔 (URL에 ?debug 붙이면 활성화)
+if (window.location.search.includes('debug')) {
+  import('eruda').then(eruda => {
+    eruda.default.init()
+  }).catch(err => {
+    console.log('Eruda load failed:', err)
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
